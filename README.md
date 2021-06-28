f9d3fb99e89b07e163fcf015c26d8072bb190722
kqwq <47221196+kqwq@users.noreply.github.com>
2021-06-28 15:53:37 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Speaker prediction
+
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function App()
+{
+    let siteName = "Grace Life CC Prediction"
+    let a = <h1>Welcome to {siteName}</h1>
+    return a
+}
